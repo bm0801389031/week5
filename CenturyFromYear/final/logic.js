3b29b18ea6550aac303eb63d8e3741a5cfb0e185
Brian Monge <bm0801389031@gmail.com>
2021-02-02 12:23:31 -0700

    final and setpu folders and files

---
+++ CenturyFromYear/final/logic.js
@@ -0,0 +1,11 @@
+/*
+variable with identifier century = assignment operator, year parameter
+with the following expression Math.ceil() function with parameter
+year devided by 100
+*/
+
+const century = year => Math.ceil(year/100)
+
+
+/*An arrow function expression is a compact alternative to traditional
+function expression, but is limited and can't be used in all situations.
