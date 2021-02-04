07310ad776750ee224a36533d72f83a5ff42a7f7
Brian Monge <bm0801389031@gmail.com>
2021-02-04 11:05:15 -0700

    setup final files done

---
+++ keephydrated/final/logic.js
@@ -0,0 +1,8 @@
+//function declaration named litres with parameter time
+function litres(time) {
+/*return Math.floor() with three paramters time, multiplicatio sign,
+and 0.5*/
+	return Math.floor(time * 0.5);
+}
+
+//very simple function
---
+++ keephydrated/setup/logic.js
@@ -0,0 +1,6 @@
+//function declaration named litres with parameter time
+function litres(time) {
+
+}
+
+//very simple function
