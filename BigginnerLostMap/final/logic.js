37b4e1482c3ac5ac0a85ce87758e6e9bf6965f2d
Brian Monge <bm0801389031@gmail.com>
2021-02-05 13:01:36 -0700

    fixed bug maps not map for function name

--- BigginnerLostMap/final/logic.js
+++ BigginnerLostMap/final/logic.js
@@ -1,5 +1,5 @@
 //declare function named maps with parameter x
-function map(x){
+function maps(x){
 /*return map method with x property[map targets each element
 in the array with the provided function, and creates a new array]
 (function name n fatarrow to declare function n times 2);*/
